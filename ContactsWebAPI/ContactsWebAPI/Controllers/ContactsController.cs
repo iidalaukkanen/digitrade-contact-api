57c422026d5b4621abdc61cf99d84940cc9ced16
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-06-05 10:20:24 +0300

    ContactsWebAPI
    ContactsController finished

--- ContactsWebAPI/ContactsWebAPI/Controllers/ContactsController.cs
+++ ContactsWebAPI/ContactsWebAPI/Controllers/ContactsController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ContactsWebAPI.Models;
+using ContactsWebAPI.Repositories;
+using ContactsWebAPI.Services;
+
 
 namespace ContactsWebAPI.Controllers
 {
@@ -11,5 +15,46 @@ namespace ContactsWebAPI.Controllers
     [ApiController]
     public class ContactsController : ControllerBase
     {
+        private readonly IContactRepository _contactRepository;
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactRepository contactRepository, IContactService contactService)
+        {
+            _contactRepository = contactRepository;
+            _contactService = contactService;
+        }
+
+        [HttpPost]
+        public ActionResult<Contact> Post(Contact contact)
+        {
+            var newContact = _contactService.Create(contact);
+            return new JsonResult(newContact);
+        }
+
+        [HttpGet]
+        public ActionResult<List<Contact>> GetContacts()
+        {
+            return new JsonResult(_contactService.Read());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Contact> Get(int id)
+        {
+            var contact = _contactService.Read(id);
+            return new JsonResult(contact);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Contact> Put(int id, Contact contact)
+        {
+            var updateContact = _contactService.Update(id, contact);
+            return new JsonResult(updateContact);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return _contactService.Delete(id);
+        }
     }
 }
